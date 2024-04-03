9313f64f5ab1eb0e4eccbc7d08f548ae4a43079b
BrunoConceica0 <bd687353@gmail.com>
2024-04-02 21:46:09 -0300

    adiconado Modal.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const vm = new Vue({
   el: "#app",
   data: {
     produtos: [],
+    produto: false,
   },
   filters: {
     formatarValorReal(valor) {
@@ -12,6 +13,7 @@ const vm = new Vue({
     },
   },
   methods: {
+    // ao click no item do produto, aparecera mais informação do produto selecionado,
     fetchProdutos() {
       fetch("./api/produtos.json").then((response) =>
         response.json().then((response) => {
@@ -19,7 +21,29 @@ const vm = new Vue({
         })
       );
     },
+    // esta função ela ira puxar somente o id do produto selecionado, para obter os dados do item, por isso tem um paramentro de id e ao puxar o fetch pegar todas as informação do  único item
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`).then((response) =>
+        response.json().then((response) => {
+          this.produto = response;
+        })
+      );
+    },
+    //Reaproveitado o fetchProduto, ao click em algum item, o site subira ao top, fazendo que, quando o usuário estive em abaixo do site, ele vera o conteúdo pela metade para o modal esta fixado em cima, quando ao clicar o produto, ele subira e vera o conteúdo por completo.
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+
+    // O evento outside pode ser feito quando o dois elementos de target e currentTarget forem igual ao click, quando é clicado com target, ele vai pegar o section do elemento, por tanto o currentTarget ele vai pegar o que exatamento foi clicado, se for acaso der um click para fora do container, ele vai pegar o section do elemento. Pode destruturar o paramentro, não percisa necessariamente usar o event
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) this.produto = false;
+    },
   },
+  // mostra os dados e valor em precisa um de evento antes, ele já é populado quado iniciamos a site
   created() {
     this.fetchProdutos();
   },
